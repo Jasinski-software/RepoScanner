9212030a2174a8640e9f85d9b65ff85a422b48d7
Lucas <lucas-software@proton.me>
2024-09-02 11:21:31 +0000

    Added if/else to chcek is user exists

--- src/main/java/com/example/demo/RepoScannerApplication.java
+++ src/main/java/com/example/demo/RepoScannerApplication.java
@@ -8,6 +8,7 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Scanner;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -39,39 +40,44 @@ public class RepoScannerApplication implements CommandLineRunner{
 				.build();
 		
 		HttpResponse<String> response = client.send(reposRequest, BodyHandlers.ofString());
+		if(response.body().charAt(0) == '[') {
+			System.out.println("Object is and JSONArray");
+			JSONArray repos = new JSONArray(response.body());
+			System.out.println("User "+ gitUser.userName + " has " + repos.length() + " repositories.\n");
+			for(int x = 0, idx = 0; x < repos.length(); x++) {
 				
-		JSONArray repos = new JSONArray(response.body());
-		System.out.println("User "+ gitUser.userName + " has " + repos.length() + " repositories.\n");
-		
-		
-		for(int x = 0, idx = 0; x < repos.length(); x++) {
-			
-			if (repos.getJSONObject(x).get("fork").toString() == "false") {
-				gitUser.repositories.add(repos.getJSONObject(x).get("name").toString());
-								
-				HttpRequest newRequest = HttpRequest.newBuilder()
-						.uri(new URI("https://api.github.com/repos/" + gitUser.userName + "/" + gitUser.repositories.get(idx) + "/branches"))
-						.GET()
-						.build();
-				HttpResponse<String> newResponse = client.send(newRequest, BodyHandlers.ofString());
-				
-				JSONArray branches = new JSONArray(newResponse.body());
-				System.out.println("Repository: " + gitUser.repositories.get(idx) + " has " + branches.length() + " branch(es):");
-				
-				for(int y = 0; y < branches.length(); y++) {
-					String branchName = branches.getJSONObject(y).get("name").toString();					
-					String chuj = branches.getJSONObject(y).getJSONObject("commit").get("sha").toString();
+				if (repos.getJSONObject(x).get("fork").toString() == "false") {
+					gitUser.repositories.add(repos.getJSONObject(x).get("name").toString());
+									
+					HttpRequest newRequest = HttpRequest.newBuilder()
+							.uri(new URI("https://api.github.com/repos/" + gitUser.userName + "/" + gitUser.repositories.get(idx) + "/branches"))
+							.header("Accept", "application/json")
+							.GET()
+							.build();
+					HttpResponse<String> newResponse = client.send(newRequest, BodyHandlers.ofString());
 					
+					JSONArray branches = new JSONArray(newResponse.body());
+					System.out.println("Repository: " + gitUser.repositories.get(idx) + " has " + branches.length() + " branch(es):");
 					
-					System.out.println("    branch: " + branchName);
-					System.out.println("    SHA: " + chuj + "\n");
+					for(int y = 0; y < branches.length(); y++) {
+						String branchName = branches.getJSONObject(y).get("name").toString();					
+						String commitSha = branches.getJSONObject(y).getJSONObject("commit").get("sha").toString();
+						
+						
+						System.out.println("    branch: " + branchName);
+						System.out.println("    SHA: " + commitSha + "\n");
+					}
+					idx++;
 				}
-				idx++;
+				else {
+					System.out.println("Repository: " + repos.getJSONObject(x).get("name") + " is a FORK.");
+				}
 			}
-			else {
-				System.out.println("Repository: " + repos.getJSONObject(x).get("name") + " is a FORK.");
-			}
-			
+		}
+		else {
+			JSONObject userResponse = new JSONObject(response.body());
+			System.out.println(userResponse.get("status"));
+			System.out.println(userResponse.get("message"));
 		}		
 	}
 }
